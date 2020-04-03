94462a665c6158c7d54c4a2827d8aebfba428554
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-04-04 00:57:25 +0530

    added autostart support

--- dwm.c
+++ dwm.c
@@ -196,6 +196,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void run(void);
+static void runAutostart(void);
 static void scan(void);
 static int sendevent(Client *c, Atom proto);
 static void sendmon(Client *c, Monitor *m);
@@ -1393,6 +1394,12 @@ run(void)
 }
 
 void
+runAutostart(void) {
+	system("cd ~/.config/dwm; ./autostart_blocking.sh");
+	system("cd ~/.config/dwm; ./autostart.sh &");
+}
+
+void
 scan(void)
 {
 	unsigned int i, num;
@@ -2211,6 +2218,7 @@ main(int argc, char *argv[])
 		die("pledge");
 #endif /* __OpenBSD__ */
 	scan();
+	runAutostart();
 	run();
 	cleanup();
 	XCloseDisplay(dpy);
