fcbbdc56c3edd9154c827c23cdaa630a06a79743
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-05-16 02:03:54 +0530

    added border colors

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -28,7 +28,7 @@ static const char col_blue[]       = "#2b70f1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] =	 { col_gray3, col_black,  col_gray2 },
-	[SchemeSel]  =	 { col_gray4, col_gray1,   col_cyan },
+	[SchemeSel]  =	 { col_gray4, col_gray1,   col_red },
 	[SchemeWarn] =	 { col_black, col_yellow, col_red },
 	[SchemeUrgent]=	 { col_white, col_red,    col_red },
 	[SchemeBlueFg] = { col_blue, col_black,  col_gray2 },
