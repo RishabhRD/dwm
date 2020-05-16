031a6c07e2a9f6104f563e20133333978f19189f
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-05-16 06:32:04 +0530

    scratchpad support for mod+tab

--- view.c
+++ view.c
@@ -9,27 +9,38 @@ shiftview(const Arg *arg)
 	Arg a;
 	Client *c;
 	unsigned visible = 0;
-	int i = arg->i;
 	int count = 0;
 	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
 
-	do {
-		if(i > 0) // left circular shift
-			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
-
-		else // right circular shift
-			nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
-
-                // Check if tag is visible
-		for (c = selmon->clients; c && !visible; c = c->next)
+	if(!(curseltags && !(curseltags & (curseltags - 1)))) return;
+	nextseltags = curseltags;
+	do{
+		count++;
+		nextseltags = nextseltags << 1;
+		for (c = selmon->clients; c && !visible; c = c->next){
 			if (nextseltags & c->tags) {
 				visible = 1;
 				break;
 			}
-		i += arg->i;
-	} while (!visible && ++count < 10);
+		}
+	} while((nextseltags <= LENGTH(tags))  && (!visible));
 
-	if (count < 10) {
+	if(!visible)
+		nextseltags = 1;
+	while(count<9 && (nextseltags < curseltags) && (!visible)){
+		count++;
+		for (c = selmon->clients; c && !visible; c = c->next){
+			if (nextseltags & c->tags) {
+				visible = 1;
+				break;
+			}
+		}
+		if(visible) break;
+		nextseltags = nextseltags << 1;
+	}
+
+
+	if (count < 9) {
 		a.i = nextseltags;
 		view(&a);
 	}
