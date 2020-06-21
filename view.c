8ff77f98870a7f6d1124f846e6978da6c2a31b23
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-06-22 03:39:56 +0530

    mod-tab to switch tags works perfectly now!!

--- view.c
+++ view.c
@@ -8,31 +8,50 @@ shiftview(const Arg *arg)
 {
 	Arg a;
 	Client *c;
-	unsigned visible = 0;
-	int i = arg->i;
-	int count = 0;
-	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
-
-	do {
-		if(i > 0) // left circular shift
-			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
-
-		else // right circular shift
-			nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i));
-
-                // Check if tag is visible
-		for (c = selmon->clients; c && !visible; c = c->next)
-			if (nextseltags & c->tags) {
-				visible = 1;
-				break;
-			}
-		i += arg->i;
-	} while (!visible && ++count < 10);
-
-	if (count < 10) {
-		a.i = nextseltags;
-		view(&a);
+	/* int i = arg->i; */
+	/* int nextseltags; */
+	int occ = 0;
+	int curseltags = selmon->tagset[selmon->seltags];
+	int curIndex;
+	int i;
+	int max = 0;
+	for(curIndex = 0;curIndex<LENGTH(tags); curIndex++){
+		if ((1<<curIndex)&curseltags){
+			max = curIndex;
+		}
 	}
+	for (c = selmon->clients; c; c = c->next) {
+		occ |= c->tags;
+	}
+	for(i = 0; i < LENGTH(tags); i++){
+		max++;
+		if(!(occ & (1 << (max % LENGTH(tags))) || selmon->tagset[selmon->seltags] & (1 << (max % LENGTH(tags)))))
+			continue;
+		a.ui = 1 << (max % LENGTH(tags));
+		view(&a);
+		break;
+		
+	}
+	/* do { */
+	/* 	if(i > 0) // left circular shift */
+	/* 		nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i)); */
+
+	/* 	else // right circular shift */
+	/* 		nextseltags = curseltags >> (- i) | (curseltags << (LENGTH(tags) + i)); */
+
+                /* // Check if tag is visible */
+	/* 	for (c = selmon->clients; c && !visible; c = c->next) */
+	/* 		if (nextseltags & c->tags) { */
+	/* 			visible = 1; */
+	/* 			break; */
+	/* 		} */
+	/* 	i += arg->i; */
+	/* } while (!visible && ++count < 10); */
+
+	/* if (count < 10) { */
+	/* 	a.i = nextseltags; */
+	/* 	view(&a); */
+	/* } */
 }
 
 void
