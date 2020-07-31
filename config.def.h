7ac8d555ebf7932d00a51a30c7ba0ee4fa75756c
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-07-31 14:17:05 +0530

    removed fast gaps shortcuts

--- config.def.h
+++ config.def.h
@@ -128,8 +128,6 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	{ MODKEY|ShiftMask, 		XK_d,      setgaps,        {.i = 10  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
