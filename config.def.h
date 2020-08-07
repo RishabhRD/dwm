2edd6b22299c6ee9fde01579ba379abe031326d8
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-08-07 22:40:32 +0530

    added restart sig patch

--- config.def.h
+++ config.def.h
@@ -110,6 +110,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY, 						XK_F2,     quit,           {1} }, 
 };
 
 /* button definitions */
