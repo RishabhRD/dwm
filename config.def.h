811295ef274938bc20741e3a3071cc7158f8f947
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-12-03 02:24:57 +0530

    Keymapping change as Tab key is swapped with equal key

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=9" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -90,7 +90,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ Mod1Mask,                     XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    shiftview,      {.i = +1} },
+	{ MODKEY,                       XK_equal,    shiftview,      {.i = +1} },
 	{ MODKEY,                       XK_grave,  view,           {0} },
 	{ MODKEY, 			            XK_o,      minOpen,        {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
